e58ed53d463ff9a11e9e9349fc0356b4472e0940
Asliddin <asliddintukhtasinov5@gmail.com>
2023-02-13 14:57:00 +0500

    19-send message by email backend part is update

--- main/signals.py
+++ main/signals.py
@@ -9,10 +9,15 @@ from .utils import send_email_with_attachment
 @receiver(post_save, sender=EmailMessages)
 def send_email_on_post_creation(sender, instance, created, **kwargs):
     if created:
+        message_text = f"FISH/Tashkilot nomi: {instance.name}\n" \
+                       f"E-mail: {instance.email}\n" \
+                       f"Tel: {instance.phone_number}\n" \
+                       f"Xabbar: {instance.message}\n" \
+                       f"Yuborilgan vaqti:{instance.created_add}"
 
         send_email_with_attachment(
-            subject=instance.title,
-            message=f"Tashkilot nomi: {instance.name_organization}\nFISH: {instance.full_name}\nE-mail: {instance.email}\nTel: {instance.phone_number}\nXabar: {instance.message}",
+            subject=f"FISH/Tashkilot nomi: {instance.name}",
+            message=message_text,
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[instance.services.email],
             file_path=instance.file.path,
